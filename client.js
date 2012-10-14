a93af96a87e20598ec5309c36096b55bf28e4d36
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2012-10-14 14:12:08 +0200

    Wow, now it works!

---
+++ client.js
@@ -0,0 +1,291 @@
+var user, users, userCount, chatDisplayed,
+    lastMessage, ajaxConnect, ajaxPoll;
+
+function init() {
+  user = null;
+  users = {};
+  userCount = 0;
+  chatDisplayed = false;
+  lastMessage = new Date();
+  ajaxConnect = null;
+  ajaxPoll = null;
+}
+
+function initConnectForm(flashMessage) {
+  $("body > *").remove();
+
+  $("body").append('<div id="connect">' +
+      '<div id="intro">' +
+      'This is <em>Node Simple Chat</em>, a simple chat' +
+      'room written in javascript.' +
+      '</div>' +
+      '<form id="connectform">' +
+        '<label for="connectuser">Username</label>' +
+        '<input id="connectuser" type="text" name="user" placeholder="Type your username here" value="" />' +
+        '<input type="submit" value="Connect" />' +
+      '</form>' +
+      '<div id ="connectdiv"></div>' +
+    '</div>');
+
+  if (flashMessage != null && flashMessage != "") {
+    $("#intro").after('<div id="connectflash" class="flash">' + flashMessage + '</div>');
+  }
+
+  $("#connectform > :input:first").focus();
+
+  $("#connectform").submit(function() {
+    register($("#connectform > :input:first").val());
+    return false;
+  });
+}
+
+function removeConnectForm() {
+  $("#connect").remove();
+}
+
+function register(username) {
+  if (username == null || username == "") {
+    $("#connectdiv").text("Enter a username!").show().fadeOut(2000);
+  } else {
+    $.ajax({
+      url: "/user",
+      dataType: "json",
+      data: { user: username },
+      type: "POST",
+      complete: function(jqXHR, textStatus) {
+        if (jqXHR.status == 400) {
+          $("#connectdiv").text(jqXHR.responseText).show().fadeOut(2000);
+        } else {
+          removeConnectForm();
+          user = username;
+          connect(username);
+        }
+      }
+    });
+  }
+}
+
+function connect(user) {
+  var start = new Date();
+
+  ajaxConnect = $.ajax({
+    url: "/connect",
+    dataType: "json",
+    data: { user: user },
+    type: "POST",
+    complete: function(jqXHR, textStatus) {
+      var time = (new Date() - start);
+      if (jqXHR.status == 400) {
+        init();
+        $("#connectdiv").text(jqXHR.responseText).show().fadeOut(2000);
+      } else if (jqXHR.status == 0) {
+        if (time < 5000) {
+          close();
+          initConnectForm("Disconnected from the server");
+        } else {
+          connect(user);
+        }
+      }
+    }
+  });
+
+  if (!chatDisplayed) {
+    chatDisplayed = true;
+    initChat();
+  }
+}
+
+function initChat() {
+  $("body").append('<form id="chatform">' +
+      '<input type="text" name="msg" placeholder="Type your message here" autocomplete="off" value="" />' +
+      '<input type="submit" value="Send" />' +
+    '</form>' +
+    '<div id ="chatinfo">Users: <ul class="users"></ul></div>' +
+    '<div id ="chatdisplay"></div>');
+
+  $("#chatform > :input:first").focus();
+
+  $("#chatform").submit(function() {
+    postMessage($("#chatform > :input:first").val(), function() {
+      $("#chatform > :input:first").val("");
+    });
+    return false;
+  });
+
+  initMessages();
+}
+
+function initMessages() {
+  $.ajax({
+    url: "/messages",
+    type: "GET",
+    complete: function(jqXHR, textStatus) {
+      if (jqXHR.status == 200) {
+        var data = JSON.parse(jqXHR.responseText);
+        if ('users' in data && 'messages' in data) {
+          data.users.forEach(addUser);
+          lastMessage = new Date(data.messages[0].date);
+          data.messages.reverse().forEach(function(message) {
+            displayMessage(message, false)
+          });
+
+          pollMessages();
+        } else {
+          close();
+          initConnectForm("Error while retrieving messages: " + jqXHR.responseText);
+        }
+      } else {
+        close();
+        initConnectForm("Error while retrieving messages: " + jqXHR.responseText);
+      }
+    }
+  });
+}
+
+function pollMessages() {
+  var start = new Date();
+
+  ajaxPoll = $.ajax({
+    url: "/next-messages?d=" + lastMessage.getTime(),
+    type: "GET",
+    complete: function(jqXHR, textStatus) {
+      var time = (new Date() - start);
+
+      if (jqXHR.status == 200) {
+        var data = JSON.parse(jqXHR.responseText);
+        if ('messages' in data) {
+          lastMessage = new Date(data.messages[0].date);
+          data.messages.reverse().forEach(function(message) {
+            displayMessage(message, true)
+          });
+
+          pollMessages();
+        } else {
+          close();
+          initConnectForm("Error while retrieving messages: " + jqXHR.responseText);
+        }
+      } else if (jqXHR.status == 0) {
+        if (time < 5000) {
+          close();
+          initConnectForm("Disconnected from the server");
+        } else {
+          pollMessages();
+        }
+      } else {
+        close();
+        initConnectForm("Error while retrieving messages: " + jqXHR.responseText);
+      }
+    }
+  });
+}
+
+function postMessage(message, callback) {
+  $.ajax({
+    url: "/message",
+    dataType: "json",
+    data: { user: user, message: message },
+    type: "POST",
+    complete: function(jqXHR, textStatus) {
+      my_var = jqXHR;
+      if (jqXHR.status == 200) {
+        callback();
+      } else {
+        close();
+        initConnectForm("Error while posting message: " + jqXHR.responseText);
+      }
+    }
+  });
+}
+
+function addUser(user) {
+  var uid;
+
+  if (!(user in users)) {
+    userCount++;
+    users[user] = userCount;
+    uid = "user" + userCount;
+    $("#chatinfo .users").append('<li id="' + uid + '">' +
+      user + '</li>');
+  }
+}
+
+function deleteUser(user) {
+  var uid;
+
+  if (user in users) {
+    uid = "#user" + users[user];
+    $(uid).remove();
+    delete users[user];
+  }
+}
+
+function displayMessage(message, userChange) {
+  var usr = message.usr;
+  var msg = message.msg;
+  var date = new Date(message.date);
+
+  if (userChange == undefined) {
+    userChange = true;
+  }
+
+  if (usr == null) {
+    $("#chatdisplay").prepend('<div class="message">' +
+        '<span class="date">' + dateString(date) + '</span>'+
+        ' <span class="time">' + timeString(date) + '</span>'+
+        ' <span class="notification">' + msg + '</span>' +
+      '</div>');
+
+    if (userChange) {
+      if (message.event.type == "join") {
+        addUser(message.event.usr);
+      } else if (message.event.type == "leave") {
+        deleteUser(message.event.usr);
+      }
+    }
+  } else {
+    $("#chatdisplay").prepend('<div class="message">' +
+        '<span class="date">' + dateString(date) + '</span>'+
+        ' <span class="time">' + timeString(date) + '</span>'+
+        ' <span class="user">' + usr + '</span>' +
+        ' <span class="message">' + msg + '</span>' +
+      '</div>');
+  }
+}
+
+function close() {
+  if (ajaxConnect != null) {
+    ajaxConnect.complete = function(jqXHR, textStatus) {};
+    ajaxConnect.abort();
+  }
+  if (ajaxPoll != null) {
+    ajaxPoll.complete = function(jqXHR, textStatus) {};
+    ajaxPoll.abort();
+  }
+
+  init();
+}
+
+function timeString(date) {
+  return twoDigits(date.getHours()) +
+    ":" + twoDigits(date.getMinutes()) +
+    ":" + twoDigits(date.getSeconds());
+}
+
+function dateString(date) {
+  return twoDigits(date.getMonth()) +
+    "/" + twoDigits(date.getDate()) +
+    "/" + date.getFullYear();
+}
+
+function twoDigits(n) {
+  return (n < 10 ? "0" : "") + n;
+}
+
+$(document).ready(function () {
+  init();
+  initConnectForm("");
+});
+
+$(window).unload(function() {
+  close();
+});
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 
 var express = require('express');
 var fs = require('fs');
+var sanitize = require('validator').sanitize;
 
 var msg = require('./lib/messages');
 
@@ -21,20 +22,30 @@ app.set('server', config[app.get('env')].server);
 app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
-  res.send("Hello world!");
+  res.sendfile('index.html');
 });
 
+if (app.get('env') == 'development') {
+  app.get('/client.js', function(req, res) {
+    res.sendfile('client.js');
+  });
+
+  app.get('/style.css', function(req, res) {
+    res.sendfile('style.css');
+  });
+}
+
 app.get('/messages', function(req, res) {
-  var n = 10;
+  var n = 50;
   if ('n' in req.query) {
-    n = req.query.n;
+    n = sanitize(req.query.n).toInt();
   }
 
   msg.getLastMessages(n, function(err, docs) {
     if (err) {
       res.send(500, err);
     } else {
-      res.json({ messages: docs });
+      res.json({ messages: docs, users: getUsers() });
     }
   });
 });
@@ -42,7 +53,7 @@ app.get('/messages', function(req, res) {
 app.get('/next-messages', function(req, res) {
   var d = new Date();
   if ('d' in req.query) {
-    d.setTime(req.query.d);
+    d.setTime(sanitize(req.query.d).toInt());
   }
 
   msg.getMessagesSince(d, function(err, docs) {
@@ -58,23 +69,73 @@ app.get('/next-messages', function(req, res) {
   });
 });
 
-app.post('/connect', function(req, res) {
+app.post('/user', function(req, res) {
   var user;
   if ('user' in req.body) {
-    user = req.body.user.toString();
+    user = sanitize(req.body.user.toString()).xss();
+    user = sanitize(user).entityEncode();
+    user = sanitize(user).trim();
     if (user in users) {
       res.send(400, "User already exists");
     } else {
-      users[user] = {
-        name: user,
-        last: new Date(),
-        res: res
-      };
+      users[user] = "pending";
 
-      // When the user disconnects
-      res.on('close', function() {
-        console.log(user + ' left');
-        msg.notify(user + ' left', function(err) {
+      // free the name if something bad happened
+      setTimeout(function() {
+        if (user in users && users[user] == "pending") {
+          console.log(user + " unregistered");
+          delete users[user];
+        }
+      }, 5000);
+
+      console.log(user + " registered");
+      res.send(200, "User name accepted");
+    }
+  } else {
+    res.send(400, "Provide user name", function() {});
+  }
+});
+
+app.post('/connect', function(req, res) {
+  var user, event;
+
+  if ('user' in req.body) {
+    user = sanitize(req.body.user.toString()).xss();
+    user = sanitize(user).entityEncode();
+    user = sanitize(user).trim();
+
+    if (user in users) {
+      if (users[user] == "pending") {
+        users[user] = {
+          name: user,
+          last: new Date(),
+          res: res
+        };
+
+        // When the user disconnects
+        res.on('close', function() {
+          users[user] = "waiting";
+
+          setTimeout(function() {
+            var event = { type: 'leave', usr: user };
+            if (user in users && users[user] == "waiting") {
+              console.log(user + ' left');
+              msg.notify(user + ' left', event, function(err) {
+                if (err) {
+                  console.log(err);
+                }
+
+                // Notify polling clients
+                notify();
+              });
+              delete users[user];
+            }
+          }, 2000);
+        });
+
+        console.log(user + ' joined');
+        event = { type: 'join', usr: user };
+        msg.notify(user + ' joined', event, function(err) {
           if (err) {
             console.log(err);
           }
@@ -82,29 +143,55 @@ app.post('/connect', function(req, res) {
           // Notify polling clients
           notify();
         });
-        delete users[user];
-      });
+      } else if (users[user] == "waiting") {
+        users[user] = {
+          name: user,
+          last: new Date(),
+          res: res
+        };
 
-      console.log(user + ' joined');
-      msg.notify(user + ' joined', function(err) {
-        if (err) {
-          console.log(err);
-        }
+        // When the user disconnects
+        res.on('close', function() {
+          users[user] = "waiting";
 
-        // Notify polling clients
-        notify();
-      });
+          setTimeout(function() {
+            var event = { type: 'leave', usr: user };
+
+            if (user in users && users[user] == "waiting") {
+              console.log(user + ' left');
+              msg.notify(user + ' left', event, function(err) {
+                if (err) {
+                  console.log(err);
+                }
+
+                // Notify polling clients
+                notify();
+              });
+              delete users[user];
+            }
+          }, 2000);
+        });
+      } else {
+        res.send(400, "User already exists");
+      }
+    } else {
+      res.send(400, "Register name first");
     }
   } else {
-    res.send(400, "Provide user name", function() {});
+    res.send(400, "Provide user name");
   }
 });
 
 app.post('/message', function(req, res) {
   var user, message;
+
   if ('user' in req.body && 'message' in req.body) {
-    user = req.body.user.toString();
-    message = req.body.message.toString();
+    user = sanitize(req.body.user.toString()).xss();
+    user = sanitize(user).entityEncode();
+    user = sanitize(user).trim();
+    message = sanitize(req.body.message.toString()).xss();
+    message = sanitize(message).entityEncode();
+    message = sanitize(message).trim();
     if (user in users) {
       msg.post(user, message, function(err) {
         if (err) {
@@ -114,6 +201,8 @@ app.post('/message', function(req, res) {
         // Notify polling clients
         notify();
       });
+
+      console.log(user + ' posted: ' + message);
       res.send(200);
     } else {
       res.send(400, "Unknown user");
@@ -127,8 +216,8 @@ msg.connectToDb(config.development.database, function(err) {
   if (err) {
     console.log(err);
   } else {
-    app.listen(3000);
-    console.log('Listening on port 3000');
+    app.listen(app.get('server').port);
+    console.log('Listening on port ' + app.get('server').port);
   }
 });
 
@@ -146,4 +235,14 @@ function notify() {
   }
 
   defers = [];
+}
+
+function getUsers() {
+  var names = [];
+  for (user in users) {
+    if (users[user] != "pending") {
+      names.push(user);
+    }
+  }
+  return names;
 }
